a4c71305d745639608341329615f6b2b28f43912
maxefimov <efimov.top.academy@gmail.com>
2025-04-02 21:08:15 +0300

    Добавьте файлы проекта.

---
+++ CppFunctionsProject/CppFunctionsProject.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+
+void PrintStars(int length = 10)
+{
+    for (int i{}; i < length; i++)
+        std::cout << "*";
+
+    std::cout << "\n";
+}
+
+int Fibonachi(int n)
+{
+    int f0{ 0 };
+    int f1{ 1 };
+
+    for (int i{ 1 }; i < n; i++)
+    {
+        int s = f0 + f1;
+        f0 = f1;
+        f1 = s;
+    }
+
+    return f1;
+}
+
+void Snowflake(int size)
+{
+    if (!(size & 1)) 
+        size++;
+
+    for (int i{}; i < size; i++)
+    {
+        for (int j{}; j < size; j++)
+        {
+            if (i == j
+                || i == size - j - 1
+                || i == size / 2
+                || j == size / 2)
+                std::cout << "* ";
+            else
+                std::cout << "  ";
+        }
+        std::cout << "\n";
+    }
+}
+
+int main()
+{
+    /*char str[] = "Hello world!";
+    int length = std::strlen(str);
+
+    PrintStars();
+    std::cout << str << "\n";
+    PrintStars();*/
+
+    //std::cout << Fibonachi(10) << "\n";
+
+    Snowflake(14);
+}
---
+++ CppFunctionsSecondProject/CppFunctionsSecondProject.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+int globalSize = 2000;
+int Counter();
+void func(int);
+
+
+int main()
+{
+    /*int a{ 123 };
+    const int maxSize{ 1000 };
+    func(a);*/
+    for (int i{}; i < 5; i++)
+        std::cout << Counter() << "\n";
+
+}
+
+
+
+void func(int b)
+{
+    int c;
+
+    Counter();
+
+}
+
+int Comparator(int a, int b)
+{
+    int c = a - b;
+    return c;
+}
+
+int Counter()
+{
+    static int count;
+    count = 0;
+
+    return ++count;
+}
